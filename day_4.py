e4e7d3a705ec473c5a03879dc295d0e87931ed79
Kurt Maurer <kurtrm@gmail.com>
2018-12-04 20:20:10 -0800

    Finished part 1.

---
+++ day_4.py
@@ -0,0 +1,58 @@
+"""
+"""
+from datetime import datetime
+from itertools import groupby
+
+from read_file import read_input
+
+
+def read_and_sort():
+    """
+    """
+    file = read_input('input_4.txt')[:-1]
+    sorted_logs = sorted(file, key=lambda x: datetime.strptime(x[1:17],
+                                                               '%Y-%m-%d %H:%M'))
+    records = [(datetime.strptime(event[1:11], '%Y-%m-%d'), event[12:17], event[19:]) for event in sorted_logs]
+    grouped_records = groupby(records, lambda x: x[0])
+    return grouped_records
+
+
+def get_activity():
+    """
+    """
+    guards = {}
+    grouped_records = read_and_sort()
+    for day, times in grouped_records:
+        for event in times:
+            if event[2].startswith('G'):
+                guard_id = int(''.join(x for x in event[2][7:13] if x.isdigit()))
+            elif event[2].startswith('f'):
+                sleep_start = int(event[1][3:])
+            else:
+                sleep_end = int(event[1][3:])
+                try:
+                    for x in range(sleep_start, sleep_end):
+                        guards[guard_id][x] += 1
+                except KeyError:
+                    guards[guard_id] = {i: 0 for i in range(0, 60)}
+                    for x in range(sleep_start, sleep_end):
+                        guards[guard_id][x] += 1
+    return guards
+
+
+if __name__ == '__main__':
+    guards_sleep = get_activity()
+    best_min = 0
+    top_guard = None
+    for key, value in guards_sleep.items():
+        highest = sum(value.values())
+        if highest > best_min:
+            top_guard = key
+    highest_count = 0
+    highest_min = 0
+    for key, value in guards_sleep[top_guard].items():
+        if value > highest_count:
+            highest_count = value
+            highest_min = key
+
+    print(top_guard, highest_min)
